e44e5d58f8b85b2258052125c3fd09684f4b19bd
Gerasimov Dmitry <gerasimov-dmr@yandex.ru>
2020-02-11 15:23:33 +0300

    for heroku 2

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,4 @@
-export const hostname = process.env.HOST || 'localhost';
-export const port = process.env.PORT || '3000';
-
 export const environment = {
   production: false,
-  apiUrl: `http://${hostname}:${port}/`,
+  apiUrl: 'https://backend-for-lab.herokuapp.com',
 };
